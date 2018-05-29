ac683f345591784e726223f1915ea56cb3e9a791
JT W <jwolohan@gmail.com>
2018-05-29 12:25:26 -0400

    Added parse-entry, parse-many, and parse-file

--- src/bib_parser/core.clj
+++ src/bib_parser/core.clj
@@ -1,14 +1,25 @@
 (ns bib-parser.core
   (:gen-class))
 
-(defn parse
-  "Parses a .bib file"
+(defn parse-entry
+  "Parses a single .bib entry"
   [s]
-  (let [entry (re-find #"(?s)@(?<bibtype>\w+?)\{(?<body>.*)\}" s)]
-    (reduce (fn [a b] (assoc a (b 1) (b 2))) {}
-    (re-seq #"(?s)(\w+)=\{(.*?)\},?" (entry 2))
-)))
+    (reduce
+      (fn [a b] (assoc a (b 1) (b 2)))
+      {}
+      (re-seq #"(?s)(\w+)=\{(.*?)\},?" s)))
 
+(defn parse-many
+  "Parses many entries"
+  [s]
+  (map 
+     #(parse-entry (get % 1 ""))
+     (re-seq #"(?s)@\w+\{(.*?)\}\s\}" s)))
+
+(defn parse-file
+  "Parses a .bib file"
+  [fp]
+  (parse-many (slurp fp)))
 
 (defn -main
   "I don't do a whole lot ... yet."
