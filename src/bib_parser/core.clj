1d928bf3c48cb81a1aad202c248293e9a391e177
JT W <jwolohan@gmail.com>
2018-05-29 12:01:43 -0400

    Single entry parse

--- src/bib_parser/core.clj
+++ src/bib_parser/core.clj
@@ -1,6 +1,15 @@
 (ns bib-parser.core
   (:gen-class))
 
+(defn parse
+  "Parses a .bib file"
+  [s]
+  (let [entry (re-find #"(?s)@(?<bibtype>\w+?)\{(?<body>.*)\}" s)]
+    (reduce (fn [a b] (assoc a (b 1) (b 2))) {}
+    (re-seq #"(?s)(\w+)=\{(.*?)\},?" (entry 2))
+)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
